5fe17b75e405fd2944119f4fa27ced42f842247e
lunghyun <nangmannsh@gmail.com>
2025-12-29 15:44:23 +0900

    di에서 별칭 제거

--- app.go
+++ app.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	student2 "github.com/lunghyun/go-api-template/internal/domain/student"
+	"github.com/lunghyun/go-api-template/internal/domain/student"
 )
 
 // MakeWebHandler 웹 핸들러 초기화
@@ -10,9 +10,9 @@ func MakeWebHandler() *gin.Engine {
 	engin := gin.Default()
 
 	// di
-	studentRepo := student2.NewMemRepository()
-	studentService := student2.NewService(studentRepo)
-	studentHandler := student2.NewHandler(studentService)
+	studentRepo := student.NewMemRepository()
+	studentService := student.NewService(studentRepo)
+	studentHandler := student.NewHandler(studentService)
 
 	// 각 도메인 라우팅 등록
 	studentHandler.RegisterRoutes(engin)
